6f886e8db280c869ad60aeb26e3490dcb2dd784b
Dan Holodak <danholodak@gmail.com>
2023-02-15 17:10:37 -0500

    phase4 complete

--- first_routes_controllers/app/controllers/users_controller.rb
+++ first_routes_controllers/app/controllers/users_controller.rb
@@ -5,10 +5,43 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        @user = User.new(strong_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: 426
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+
+        render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(strong_params)
+            redirect_to user_url(@user.id)
+        else
+            render json: @user.errors.full_messages, status: 403
+        end
+
+
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user.destroy
+            render json: "USER DESTROYED!!!"
+        else
+            render json: @user.errors.full_messages, status: 417
+        end
+    end
+
+    private
+
+    def strong_params
+        params.require(:user).permit(:name, :email)
     end
 end
