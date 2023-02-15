288e2b1d053de057ccc52cfeb3b57cdad343dfb9
Dan Holodak <danholodak@gmail.com>
2023-02-15 16:21:00 -0500

    made some very useful controllers

--- first_routes_controllers/app/controllers/users_controller.rb
+++ first_routes_controllers/app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
     def index
         render plain: "Hello world"
     end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
 end
